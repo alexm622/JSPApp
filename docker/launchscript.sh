74e998e338a3fed33655287c3e85ce5defc9b343
Alex <longnamesrule@gmail.com>
2019-12-13 13:21:09 -0500

    trying to get serverstatus to work in background

--- docker/launchscript.sh
+++ docker/launchscript.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 echo "hello"
 service ssh start
-java -jar /usr/local/tomcat/ServerStatusReader.jar
+java -jar /usr/local/tomcat/ServerStatusReader.jar &
 #catalina.sh run
+while [ True]
+do
+   cat /tmp/servervars/serverstatus.bin
+   sleep 10
+done
