b9c0ae7756a550c29bad91e5d5b5c09f161321f5
Alex <longnamesrule@gmail.com>
2019-12-13 13:33:51 -0500

    i think it works now...

--- docker/launchscript.sh
+++ docker/launchscript.sh
@@ -2,10 +2,5 @@
 echo "hello"
 #service ssh start
 java -jar /usr/local/tomcat/ServerStatusReader.jar &
-#catalina.sh run
-while :
-do
-	echo "refreshing"
-	cat /tmp/servervars/serverstatus.bin
-	sleep 10
-done
+catalina.sh run
+
