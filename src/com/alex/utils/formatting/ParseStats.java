80a2276cd47a72202d17502b1ad42704dba19c12
alexm622 <longnamesrule@gmail.com>
2020-01-11 12:31:06 -0500

    added comments to the sorting function

--- src/com/alex/utils/formatting/ParseStats.java
+++ src/com/alex/utils/formatting/ParseStats.java
@@ -39,18 +39,31 @@ public class ParseStats {
 			
 			
 			for(Pair<Long, Double> p : arrayList) {
+				// get the long and doubles from the pair
 				Long l = p.getLeft();
 				Double d = p.getRight();
 				
+				// variable that will be often overwritten
 				ArrayList<Pair<String, Double>> byappid;
 				
-				if(!sorted.containsKey(l)) {
+				
+				if(!sorted.containsKey(l)) { //test if there is not an entry under that key (l)
+					//make a new arraylist containing pairs
 					byappid = new ArrayList<Pair<String,Double>>();
+					
+					//add the current value for the pair to the arraylist
 					byappid.add(new MutablePair<String, Double>(s, d));
+					
+					//put the arraylist into the hashmap
 					sorted.put(l, byappid);
-				}else {
+				}else { // if the key does exist
+					//get the value stored in the hashmap under the current key
 					byappid = sorted.get(l);
+					
+					// add the current pair value
 					byappid.add(new MutablePair<String, Double>(s, d));
+					
+					//replace the value with the updated arraylist
 					sorted.replace(l, byappid);
 				}
 				
