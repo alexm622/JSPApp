b0f540868d1f2c8b86ef257d4eeed1a809bb6714
alexm622 <longnamesrule@gmail.com>
2020-02-25 22:03:52 -0500

    new servlet and starting work on comments

---
+++ src/com/alex/servlets/forums/Comment.java
@@ -0,0 +1,41 @@
+package com.alex.servlets.forums;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+@WebServlet("/comment")
+public class Comment extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4962336299528017400L;
+	private static String content;
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// set the content attribute
+		request.setAttribute("content", content);
+		
+		//redirect
+		request.getRequestDispatcher("editor_test.jsp").forward(request,response);
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//get the content in the textarea
+		content = Jsoup.clean(request.getParameter("content"), Whitelist.basic());
+		
+		//execute a GET function
+		response.sendRedirect(request.getRequestURI());
+	}
+}
